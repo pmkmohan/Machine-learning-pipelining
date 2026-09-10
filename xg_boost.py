ab209bf41039bad28024f357b77b4f23e1f1033e
Mohan Patharapilli <pmk.mohan@gmail.com>
2026-09-10 16:45:23 +0530

    Update xg_boost.py

--- xg_boost.py
+++ xg_boost.py
@@ -33,7 +33,7 @@ X = np.array(ct.fit_transform(X))
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+# pip install xgboost
 # Training XGBoost on the Training set
 from xgboost import XGBClassifier 
 classifier = XGBClassifier() 
